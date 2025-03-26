901a0710d77a9ffa70790f4f50fa00c4187e16d7
Jose A. Sánchez <jsanchez.works@gmail.com>
2025-03-26 13:02:23 +0100

    Add styles.css

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,7 +5,14 @@ export const GifExpertApp = () => {
   const apiKey = 'rPmkD9LhBgMLCBpps3DJJ0tjNnl0KiLv';
   return (
     <>
+        {/* titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+
+
+        {/* Listado de Gig */}
+            {/* Gif Item */}
     </>
   )
 }
